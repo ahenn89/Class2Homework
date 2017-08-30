5f05d21dc7d20f44ab0e504528c89fc138c72f74
Ashlee <Ashlee@Ashlee-PC>
2017-08-30 15:46:57 -0500

    encapsulation & method

--- src/class2homework/Class2Homework.java
+++ src/class2homework/Class2Homework.java
@@ -5,9 +5,46 @@ package class2homework;
  * @author Ashlee
  */
 public class Class2Homework {
-
-    public static void main(String[] args) {
-        // TODO code application logic here
+    private String firstName;
+    private String lastName;
+    private int age;
+    
+    //Get
+    public String getfirstName() {
+        return firstName;
     }
+    
+    public String getlastName() {
+        return lastName;
+    }
+    
+    public int getAge() {
+        return age;
+    }
+    //set
+    public void setfirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setlastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+   public void johnObject() {
+        firstName = "John";
+        lastName = "Object";
+        age = 25;
+   }
+   
+   public void maryObject() {
+       firstName = "Mary";
+       lastName = "Object";
+       age = 26;
+   }
+   
     
 }
